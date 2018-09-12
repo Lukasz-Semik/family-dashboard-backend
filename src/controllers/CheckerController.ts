64409d5271de8540445b3299cb8d0c6f6a5ce8db
djpluki <djpluki@gmail.com>
2018-09-12 08:34:17 +0200

    start resolving problme

--- src/controllers/CheckerController.ts
+++ src/controllers/CheckerController.ts
@@ -1,5 +1,7 @@
 import { JsonController, Body, Get, Post, Req, Res, UseBefore } from 'routing-controllers';
+import { getRepository } from 'typeorm';
 
+import { User } from '../entity/User';
 import urlencodedParser from '../utils/bodyParser';
 
 @JsonController()
@@ -7,5 +9,14 @@ export class UserController {
   @Get('/hello')
   getAll(@Req() req: any, @Res() res: any) {
     return res.status(200).json({ hello: 'hello' });
+  }
+
+  @Post('/test')
+  @UseBefore(urlencodedParser)
+  putSomething(@Body() data: any, @Res() res: any) {
+    const userRepository = getRepository(User);
+    const user = new User();
+    user.firstname = 'tescik';
+    userRepository.save(user).then(arg => console.log(arg));
   }
 }
