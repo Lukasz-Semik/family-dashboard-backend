62188580a24bec450950aeccfdc5968eaad466dc
djpluki <djpluki@gmail.com>
2018-09-13 15:35:55 +0200

    make code improvements

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,11 @@
 import { JsonController, Body, Post, Res, UseBefore } from 'routing-controllers';
 import { getRepository } from 'typeorm';
+import { isEmpty } from 'lodash';
 
 import { User } from '../entity/User';
 import urlencodedParser from '../utils/bodyParser';
 import { API_SIGN_UP } from '../constants/routes';
-import { internalServerErrors } from '../constants/errors';
+import { internalServerErrors, emailErrors } from '../constants/errors';
 import { validateSignUp } from '../validators/user';
 
 @JsonController()
@@ -20,17 +21,30 @@ export class UserController {
     const { email, password, firstName, lastName } = body;
     const { isValid, errors } = validateSignUp(email, password, firstName, lastName);
 
-    if (!isValid) return res.status(400).json({ errors });
-
-    const user = new User();
-    user.email = email;
-    user.password = password;
-    user.firstName = firstName;
-    user.lastName = lastName;
-
     return this.userRepository
-      .save(user)
-      .then(arg => res.status(200).json(arg))
-      .catch(err => res.status(400).json({ error: internalServerErrors.sthWrong }));
+      .find({ email })
+      .then(existingUser => {
+        if (!isEmpty(existingUser)) return res.status(400).json({ email: emailErrors.emailTaken });
+
+        if (!isValid) return res.status(400).json({ errors });
+
+        const user = new User();
+
+        return this.userRepository
+          .save({
+            ...user,
+            email,
+            password,
+            firstName,
+            lastName,
+          })
+          .then(arg => res.status(200).json(arg))
+          .catch(err =>
+            res.status(400).json({ error: internalServerErrors.sthWrong, caughtError: err })
+          );
+      })
+      .catch(err =>
+        res.status(400).json({ error: internalServerErrors.sthWrong, caughtError: err })
+      );
   }
 }
