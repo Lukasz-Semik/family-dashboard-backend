11e27bf027be015329a2c90b78fe7fedb959dab4
djpluki <djpluki@gmail.com>
2018-09-14 12:31:21 +0200

    clean master

--- app/build/src/constants/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.api = '/api';
-exports.generateFullApi = suffix => `${exports.api}${suffix}`;
-exports.API_SIGN_UP = '/sign-up';
-exports.API_FULL_SIGN_UP = exports.generateFullApi(exports.API_SIGN_UP);
-//# sourceMappingURL=routes.js.map
--- app/build/src/utils/bodyParser.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bodyParser = require("body-parser");
-// create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-exports.default = urlencodedParser;
-//# sourceMappingURL=bodyParser.js.map
