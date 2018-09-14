eb1dd95741c42c66be3a742837cc6c377d995b97
djpluki <djpluki@gmail.com>
2018-09-14 10:55:54 +0200

    hotfix - test for heroku

--- config/config.js
+++ config/config.js
@@ -1,23 +1,25 @@
 const envPath = `.env.${process.env.NODE_ENV || 'development'}`;
 require('dotenv').config({ path: envPath });
 
-const REQUIRED_KEYS = [
-  'DB_TYPE',
-  'DB_HOST',
-  'DB_PORT',
-  'DB_USERNAME',
-  'DB_PASSWORD',
-  'DB_NAME',
-  'NODE_PORT',
-];
-
-REQUIRED_KEYS.forEach(key => {
-  if (!(key in process.env)) {
-    throw new Error(`Missing required config key: ${key}`);
-  }
-});
-
 const { DB_TYPE, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME, NODE_PORT } = process.env;
+
+if (NODE_PORT !== 'production') {
+  const REQUIRED_KEYS = [
+    'DB_TYPE',
+    'DB_HOST',
+    'DB_PORT',
+    'DB_USERNAME',
+    'DB_PASSWORD',
+    'DB_NAME',
+    'NODE_PORT',
+  ];
+
+  REQUIRED_KEYS.forEach(key => {
+    if (!(key in process.env)) {
+      throw new Error(`Missing required config key: ${key}`);
+    }
+  });
+}
 
 module.exports = {
   DB_TYPE,
