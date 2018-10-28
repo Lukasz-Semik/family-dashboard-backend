9ebd4002515665fab01a463936be8990c69d011b
djpluki <djpluki@gmail.com>
2018-10-28 19:44:31 +0100

    make linter happy

--- src/controllers/Token.spec.ts
+++ src/controllers/Token.spec.ts
@@ -1,16 +1,15 @@
 import { expect } from 'chai';
 
 import { Token } from '.';
-import { notSeededUsers } from '../constants/testFixtures';
 
 describe('Token', () => {
   it('should create and decode token', async () => {
-    const token = await Token.create({ email: notSeededUsers[0].email });
+    const token = await Token.create({ email: 'some@email.com' });
     const decodedToken = await Token.decode(token);
     const { email, iat, exp } = decodedToken;
 
     expect(token).to.be.a('string');
-    expect(email).to.equal(notSeededUsers[0].email);
+    expect(email).to.equal('some@email.com');
     expect(iat).to.be.a('number');
     expect(exp).to.be.a('number');
   });
